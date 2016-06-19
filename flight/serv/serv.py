4ce572e83c52ec8208a1018ac925737b4d432c32
blackberrypi <ryanjcutter@gmail.com>
2016-06-19 21:16:47 +0000

    Modified how bootup runs, made sure that serv works with clnt.

--- flight/serv/serv.py
+++ flight/serv/serv.py
@@ -29,7 +29,7 @@ class Connection():
 		self.cmdLED = cmdLED
 		self.TCP_IP = '192.168.1.234' # 127.0.0.1 works too, may need to change clnt
 		self.TCP_PORT = 8080
-		self.BUFFER_SIZE = 1024
+		self.BUFFER_SIZE = 128
 
 		# Successful bootup
 		self.downlink.put(["SV", "BU", "SERV"])
@@ -39,6 +39,7 @@ class Connection():
 
 		# Create server connection
 		self.connect()
+		print("Created Server Connection")
 
 		# Run flight loop
 		self.flight()
@@ -51,28 +52,26 @@ class Connection():
 			return False
 
 	def checkTemp(self):
-                if self.tempLED.is_set():
-                        self.tempLED.clear()
-                        return True
-                else:
-                        return False
+		if self.tempLED.is_set():
+			self.tempLED.clear()
+			return True
+		else:
+			return False
 
-        def checkCmd(self):
-                if self.cmdLED.is_set():
-                        self.cmdLED.clear()
-                        return True
-                else:
-                        return False
+	def checkCmd(self):
+		if self.cmdLED.is_set():
+			self.cmdLED.clear()
+			return True
+		else:
+			return False
 
-	def heartBeat(self, message):
-                with msgLock:
-                        self.message += ' night' if self.checkNight() else ''
-                        self.message += ' command' if self.checkCmd() else ''
-                        self.message += ' temp' if self.checkTemp() else ''
-                        #self.conn.send(message.encode())
-                        print(self.message)
-                        self.message = "heartbeat"
-		threading.Timer(5.0, self.heartBeat).start()
+	def heartBeat(self):
+		with msgLock:
+			self.message += ' night' if self.checkNight() else ''
+			self.message += ' command' if self.checkCmd() else ''
+			self.message += ' temp' if self.checkTemp() else ''
+			self.conn.send(self.message.encode())
+			self.message = "heartbeat"
 
 	def connect(self):
 		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,11 +79,18 @@ class Connection():
 		self.s.bind((self.TCP_IP, self.TCP_PORT))
 		self.s.listen(5)
 		self.conn, self.addr = self.s.accept()
+		self.downlink.put(["SV","BU","CLNT"])
 
 	def receive(self):
-		self.data = self.conn.recv(self.BUFFER_SIZE).decode()
-		if self.data != None:
-			self.downlink.put(["BL", "HB", self.data]) 
+		try:
+			data = self.conn.recv(self.BUFFER_SIZE).decode()
+			while data == None:
+				data = self.conn.recv(self.BUFFER_SIZE).decode()
+			self.downlink.put(["SV", "HB", data])
+		except:
+			self.downlink.put(["SV","BL","Connection Lost"])
+			self.connect()
+		return
 
 	def restart(self):
 		command = "/usr/bin/sudo /sbin/shutdown -r now"
@@ -92,37 +98,38 @@ class Connection():
 		self.s.close()
 
 	def command(self):
-		cmd = inputQ.get()
+		cmd = self.inputQ.get()
 		with msgLock:
-                        if cmd == b"\x01":
-                                self.message += " reboot"
-                        elif cmd == b"\x02":
-                                self.message += " ping"
-                        elif cmd == b"\x03":
-                                self.message += " cpu"
-                        elif cmd == b"\x04":
-                                self.message += " temp"
-                        elif cmd == b"\x05":
-                                self.message += " disk"
-                        elif cmd == b"\x06":
-                                self.message += " faster"
-                        elif cmd == b"\x07":
-                                self.message += " slower"
-                        elif cmd == b"\x08":
-                                self.message += " reboot"
-                                threading.Timer(7.0, self.restart).start()
-                        elif cmd == b"\x09":
-                                self.message += " image"
-                        elif len(cmd) > 0:
-                                self.downlink.put(["SV", "BL", "ER"])			
+			if cmd == b"\x01":
+				self.message += " reboot"
+			elif cmd == b"\x02":
+				self.message += " ping"
+			elif cmd == b"\x03":
+				self.message += " cpu"
+			elif cmd == b"\x04":
+				self.message += " temp"
+			elif cmd == b"\x05":
+				self.message += " disk"
+			elif cmd == b"\x06":
+				self.message += " faster"
+			elif cmd == b"\x07":
+				self.message += " slower"
+			elif cmd == b"\x08":
+				self.message += " reboot"
+				threading.Timer(7.0, self.restart).start()
+			elif cmd == b"\x09":
+				self.message += " image"
+			elif len(cmd) > 0:
+				self.downlink.put(["SV", "BL", "ER"])			
 
 	def flight(self):
 		self.downlink.put(["BL","BU","CLNT"])
-		threading.Timer(5.0, self.heartBeat).start()
+		self.message = "heartbeat"
 		while True:
-			print(self.inputQ.empty())
+			time.sleep(5) # Don't remove if you value sanity
 			if not self.inputQ.empty():
 				self.command()
+			self.heartBeat()
 			self.receive()
 
 def main(downlink, inputQ, nightMode, tempLED, cmdLED):
